34c06fd98eefb3fcb02cd1925388e91c40b7a375
Alexander F <alexanderf@indeed.com>
2026-04-24 16:27:20 -0500

    fix(cmux): add /usr/local/bin to PATH for execFile calls

--- extensions/cmux/src/cli.ts
+++ extensions/cmux/src/cli.ts
@@ -1,8 +1,13 @@
 import { execFile } from "child_process";
 
+const expandedEnv = {
+  PATH: "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin",
+  ...process.env,
+};
+
 export function execFileAsync(command: string, args: string[]): Promise<string> {
   return new Promise((resolve, reject) => {
-    execFile(command, args, { encoding: "utf8" }, (error, stdout) => {
+    execFile(command, args, { encoding: "utf8", env: expandedEnv }, (error, stdout) => {
       if (error) {
         reject(error);
         return;
